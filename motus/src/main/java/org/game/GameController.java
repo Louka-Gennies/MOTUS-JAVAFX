dd7557b21b03f13ec6b30b9411a3d66da800f5ab
Louka-Gennies <146700433+Louka-Gennies@users.noreply.github.com>
2024-04-28 19:52:13 +0200

    set up the Game scene

--- motus/src/main/java/org/game/GameController.java
+++ motus/src/main/java/org/game/GameController.java
@@ -3,6 +3,13 @@ package org.game;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.scene.text.Font;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameController {
 
@@ -13,7 +20,7 @@ public class GameController {
     private TextField wordInput;
 
     @FXML
-    private Label previousAttemptsLabel;
+    private TextFlow previousAttemptsLabel;
 
     @FXML
     private Label winOrLoseOrErrorLabel;
@@ -44,8 +51,15 @@ public class GameController {
     private void submitWord() {
         System.out.println("The word to guess is: " + wordToGuess);
         String word = wordInput.getText().toLowerCase();
+        List<Text> coloredTexts = colorLetters(wordToGuess, word);
+        for (Text text : coloredTexts) {
+            text.setFont(new Font(30));
+        }
         if (word.equals(wordToGuess)) {
+            previousAttemptsLabel.getChildren().addAll(0, coloredTexts);
+            previousAttemptsLabel.getChildren().addFirst(new Text("\n"));
             winOrLoseOrErrorLabel.setText("Congratulations! You guessed the word!");
+            wordInput.clear();
         } else {
             if (word.length() != wordToGuess.length()) {
                 winOrLoseOrErrorLabel.setText("The word must have " + wordToGuess.length() + " characters.");
@@ -55,13 +69,36 @@ public class GameController {
                 attemptsLabel.setText("Attempts remaining: " + attempts);
                 if (attempts == 0) {
                     winOrLoseOrErrorLabel.setText("You lost! The word was: " + wordToGuess);
+                    previousAttemptsLabel.getChildren().addAll(0, coloredTexts);
+                    previousAttemptsLabel.getChildren().addFirst(new Text("\n"));
                     wordInput.clear();
                 } else {
                     winOrLoseOrErrorLabel.setText("Try again!");
-                    previousAttemptsLabel.setText(previousAttemptsLabel.getText() + word + "\n");
+                    previousAttemptsLabel.getChildren().addAll(0, coloredTexts);
+                    previousAttemptsLabel.getChildren().addFirst(new Text("\n"));
                     wordInput.clear();
                 }
             }
         }
     }
+
+    public List<Text> colorLetters(String wordToGuess, String userInput) {
+        List<Text> coloredTexts = new ArrayList<>();
+        for (int i = 0; i < userInput.length(); i++) {
+            char c = userInput.charAt(i);
+            Text text = new Text(String.valueOf(c));
+
+            if (i < wordToGuess.length() && c == wordToGuess.charAt(i)) {
+                text.setFill(Color.GREEN);
+            } else if (wordToGuess.contains(String.valueOf(c))) {
+                text.setFill(Color.ORANGE);
+            } else {
+                text.setFill(Color.RED);
+            }
+
+            coloredTexts.add(text);
+        }
+        return coloredTexts;
+    }
+
 }
